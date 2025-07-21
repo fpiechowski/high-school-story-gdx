9deaf600f35f10ba4b6572d5db993b3870259078
Filip Piechowski <f.piechowski@gmail.com>
2025-07-21 15:37:37 +0200

    Sort entities by sprite Y position in SpriteRenderingSystem

--- core/src/main/kotlin/pro/piechowski/highschoolstory/sprite/SpriteRenderingSystem.kt
+++ core/src/main/kotlin/pro/piechowski/highschoolstory/sprite/SpriteRenderingSystem.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.github.quillraven.fleks.Entity
 import com.github.quillraven.fleks.IteratingSystem
 import com.github.quillraven.fleks.World.Companion.family
+import com.github.quillraven.fleks.collection.compareEntity
 import ktx.graphics.use
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -16,6 +17,13 @@ class SpriteRenderingSystem :
                 @ReadOnly CurrentSprite,
             )
         },
+        comparator =
+            compareEntity { entityA, entityB ->
+                val positionA = entityA[CurrentSprite].sprite.y
+                val positionB = entityB[CurrentSprite].sprite.y
+
+                positionB.compareTo(positionA)
+            },
     ),
     KoinComponent {
     private val spriteBatch: SpriteBatch by inject()
