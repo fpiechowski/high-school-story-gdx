e42dacec572d031be8a88058408ed42fa8369662
Filip Piechowski <f.piechowski@gmail.com>
2025-07-25 13:31:37 +0200

    Refactoring

--- core/src/main/kotlin/pro/piechowski/highschoolstory/character/rendering/CharacterTexture.kt
+++ core/src/main/kotlin/pro/piechowski/highschoolstory/character/rendering/CharacterTexture.kt
@@ -3,20 +3,18 @@
 import pro.piechowski.highschoolstory.character.animation.CharacterAnimation
 import pro.piechowski.highschoolstory.direction.Direction4
 
-class CharacterTexture {
-    companion object {
-        fun getLocomotionAnimationRegionsColumnRange(direction: Direction4) =
-            when (direction) {
-                Direction4.Down -> 18..23
-                Direction4.Left -> 12..17
-                Direction4.Right -> 0..5
-                Direction4.Up -> 6..11
-            }
+object CharacterTexture {
+    fun getLocomotionAnimationRegionsColumnRange(direction: Direction4) =
+        when (direction) {
+            Direction4.Down -> 18..23
+            Direction4.Left -> 12..17
+            Direction4.Right -> 0..5
+            Direction4.Up -> 6..11
+        }
 
-        fun getAnimationRegionsRow(animation: CharacterAnimation) =
-            when (animation) {
-                CharacterAnimation.IDLE -> 1
-                CharacterAnimation.WALK -> 2
-            }
-    }
+    fun getAnimationRegionsRow(animation: CharacterAnimation) =
+        when (animation) {
+            CharacterAnimation.IDLE -> 1
+            CharacterAnimation.WALK -> 2
+        }
 }
